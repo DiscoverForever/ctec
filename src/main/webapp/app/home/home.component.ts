2fdcb6da41dfa90c5e22a5f701135687a952b58e
tangyy <tangyueyang@lztogether.com>
2018-01-25 22:11:14 +0800

    Add echarts

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
 import * as Cropper from 'cropperjs';
+import * as Echars from 'echarts';
 @Component({
     selector: 'jhi-home',
     templateUrl: './home.component.html',
@@ -25,6 +26,7 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
         this.initCropper();
+        this.initEcharts();
         this.principal.identity().then((account) => {
             this.account = account;
         });
@@ -65,7 +67,77 @@ export class HomeComponent implements OnInit {
                 console.log(e.detail.scaleY);
             }
         });
-        cropper.crop()
-        cropper.reset()
+    }
+
+    initEcharts() {
+        // 基于准备好的dom，初始化echarts实例
+        const myChart = Echars.init(<HTMLDivElement>document.getElementById('main'));
+
+        // 指定图表的配置项和数据
+        // const option = {
+        //     title: {
+        //         text: 'ECharts 入门示例'
+        //     },
+        //     tooltip: {},
+        //     legend: {
+        //         data:['销量']
+        //     },
+        //     xAxis: {
+        //         data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        //     },
+        //     yAxis: {},
+        //     series: [{
+        //         name: '销量',
+        //         type: 'bar',
+        //         data: [5, 20, 36, 10, 10, 20]
+        //     }]
+        // };
+        const option = {
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                x: 'left',
+                data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+            },
+            series: [
+                {
+                    name:'访问来源',
+                    type:'pie',
+                    radius: ['50%', '70%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'center'
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                fontSize: '30',
+                                fontWeight: 'bold'
+                            }
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        }
+                    },
+                    data:[
+                        {value:335, name:'直接访问'},
+                        {value:310, name:'邮件营销'},
+                        {value:234, name:'联盟广告'},
+                        {value:135, name:'视频广告'},
+                        {value:1548, name:'搜索引擎'}
+                    ]
+                }
+            ]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
     }
 }
